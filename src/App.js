c2422ef758661006e0b2ae868ff2af427b018b9d
atsushi <77239082+oreojinyuu34@users.noreply.github.com>
2023-07-01 13:05:37 +0900

    ドラックアンドドロップ

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [items] = useState(["item0", "item1", "item2"]);
+  const onDragEnd = (result) => {
+    // console.log(result.source.index);
+    // console.log(result.destination.index);
+    const remove = items.splice(result.source.index, 1);
+    console.log(remove);
+    items.splice(result.destination.index, 0, remove[0]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="dragDropArea">
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              <Draggable draggableId="item0" index={0}>
+                {(provided) => (
+                  <div
+                    className="item"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    {items[0]}
+                  </div>
+                )}
+              </Draggable>
+              <Draggable draggableId="item1" index={1}>
+                {(provided) => (
+                  <div
+                    className="item"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    {items[1]}
+                  </div>
+                )}
+              </Draggable>
+              <Draggable draggableId="item2" index={2}>
+                {(provided) => (
+                  <div
+                    className="item"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    {items[2]}
+                  </div>
+                )}
+              </Draggable>
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
